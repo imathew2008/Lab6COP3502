5c71c0c2a373283ac93e1e65266f2df3ffdb00e1
Isabelle <isabellem5444@gmail.com>
2024-10-23 20:38:01 -0400

    added comments

--- main.py
+++ main.py
@@ -1,26 +1,36 @@
-#first comment
+
+#Isabelle Mathew
+
 print('hello world')
 def menu():
+    #print menu
     print(f'Menu\n-------------\n1. Encode\n2. Decode\n3. Quit')
 
 def encode(password):
-    newpassword = ''
+    #create empty string for encoded password
+    newPassword = ''
+    #iterate through password
     for char in password:
+        #take last digit +3 and add to newPassword
         new_digit = (int(char) + 3) % 10
-        newpassword += str(new_digit)
+        newPassword += str(new_digit)
 
     print("Your password has been encoded and stored!")
-    return newpassword
+    return newPassword
 
-
+#main
 if __name__ == "__main__":
-    storedPassword = "999999"
-    encodedPassword = ''
+    #initialize empty passwords
+    storedPassword = ""
+    encodedPassword = ""
+    #program loop
     while True:
+        #print menu and take menu selection
         menu()
         print("Please enter an option: ", end = '')
         pick = int(input())
 
+        #if user selected 1 ask for and encode password
         if pick == 1:
             print("Please enter your password to encode: ", end = '')
             storedPassword = input()
@@ -29,5 +39,6 @@ if __name__ == "__main__":
         #if pick == 2:
             #decode
 
+        #if user selected 3 terminate program
         if pick == 3:
             exit()
